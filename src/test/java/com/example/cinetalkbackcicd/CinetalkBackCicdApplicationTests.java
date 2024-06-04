0c3336401a6619588ca5df9c01d21f141dd76f67
Lianiscoming <121073208+Lianiscoming@users.noreply.github.com>
2024-06-04 18:16:21 +0900

    first commit

---
+++ src/test/java/com/example/cinetalkbackcicd/CinetalkBackCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cinetalkbackcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinetalkBackCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
