64e7568599fb6191e56d6cb8806aacc9b3b301e2
Lianiscoming <121073208+Lianiscoming@users.noreply.github.com>
2024-06-04 19:20:53 +0900

    docker 설정 및 docker-compose 설정

--- src/main/java/com/example/cinetalkbackcicd/CinetalkBackCicdApplication.java
+++ src/main/java/com/example/cinetalkbackcicd/CinetalkBackCicdApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CinetalkBackCicdApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(CinetalkBackCicdApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(CinetalkBackCicdApplication.class, args);
+	}
 
 }
